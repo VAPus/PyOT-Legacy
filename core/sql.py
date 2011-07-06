65961a1d71fde1735846746b2e1a6f12165ae7c9
Stian Andreassen <stian@tech1.org>
2011-07-06 00:38:42 +0000

    Fixer for sqlite3

--- core/sql.py
+++ core/sql.py
@@ -7,6 +7,19 @@ def connect():
         return adbapi.ConnectionPool(config.sqlModule, host="localhost",db=config.sqlDatabase, user=config.sqlUsername, passwd=config.sqlPassword, cp_min=config.sqlMinConnections, cp_max=config.sqlMaxConnections, cursorclass=DictCursor)
     elif config.sqlModule is "sqlite3":
         import sqlite3
-        return adbapi.ConnectionPool(config.sqlModule, database=config.sqlDatabase, check_same_thread=False, detect_types=sqlite3.PARSE_COLNAMES)
+        
+        # Implode our little hack to allow both sqlite3 and mysql to work together!
+        # This is a bit slower I guess, but it works :)
+        def runQuery(self, *args, **kw):
+            args = list(args)
+            args[0] = args[0].replace('%s', '?').replace('%f', '?').replace('%d', '?') # String, float and digit support
+            args = tuple(args)
+            return self.runInteraction(self._runQuery, *args)
+        adbapi.ConnectionPool.runQuery = runQuery
+        
+        def fixer(conn):
+            conn.row_factory = sqlite3.Row
+        return adbapi.ConnectionPool(config.sqlModule, config.sqlDatabase, isolation_level=None, cp_openfun=fixer, check_same_thread=False)
+        
 # Setup the database pool when this module is imported for the first time
 conn = connect()
