4e9bf4586702d0813e66443ebed6dcabd9495f6e
Stian Andreassen <stian@tech1.org>
2011-08-06 02:10:45 +0000

    Bugfix for empty levels Evo map

--- data/map/info.py
+++ data/map/info.py
@@ -1,8 +1,8 @@
 width = 2048
 height = 2048
 author = 'OTBMXML2sec generator'
-description = 'Vardius Custom Map Project 2011 - Age of the Dragons'
+description = 'SimOne MapEditor 0.5.0'
 sectorSize = (32, 32)
-towns = {1: ('Cadiz', [892, 1018, 7]), 2: ('Thmuis', [1021, 1238, 6]), 3: ('Eretum', [1080, 1044, 6]), 8: ('Sarepta', [1417, 1436, 7]), 9: ('Avaris', [1465, 962, 7]), 10: ('Kavala', [1607, 1290, 5]), 11: ('Hellrise', [1078, 1314, 6])}
-waypoints = {'Hello': [1000, 1000, 7]}
-levels = (16, 0)
+towns = {1: ('Enigma City', [1000, 1000, 7])}
+waypoints = {}
+levels = (13, 0)
