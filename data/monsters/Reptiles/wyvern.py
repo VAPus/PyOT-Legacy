00f23609feff78440270e453bb70c2d1e8cbf782
soul4soul <soul4soul@localhost>
2011-08-14 16:14:03 +0000

    2 more classes of monsters finished *added Reptiles *added Slimes and Blobs

---
+++ data/monsters/Reptiles/wyvern.py
@@ -0,0 +1,12 @@
+import game.monster
+
+wyvern = game.monster.genMonster("Wyvern", (239, 6302), "a wyvern")
+wyvern.setHealth(795)
+wyvern.bloodType(color="blood")
+wyvern.setDefense(armor=18, fire=1, earth=0, energy=0.8, ice=0.9, holy=1, death=1, physical=1, drown=1)
+wyvern.setExperience(515)
+wyvern.setSpeed(200)
+wyvern.setBehavior(summonable=0, attackable=1, hostile=1, illusionable=0, convinceable=0, pushable=0, pushItems=1, pushCreatures=1, targetDistance=1, runOnHealth=300)
+wyvern.walkAround(energy=1, fire=1, poison=0)
+wyvern.setImmunity(paralyze=1, invisible=1, lifedrain=1, drunk=1)
+wyvern.voices("Shriiiek")
---
+++ data/monsters/Slimes and Blobs/death blob.py
@@ -0,0 +1,11 @@
+import game.monster
+
+death_blob = game.monster.genMonster("Death Blob", (315, 9960), "a death blob")
+death_blob.setHealth(320)
+death_blob.bloodType(color="undead")
+death_blob.setDefense(armor=10, fire=1.1, earth=0, energy=1.1, ice=0.9, holy=1.1, death=0, physical=0.8, drown=1)
+death_blob.setExperience(300)
+death_blob.setSpeed(230)
+death_blob.setBehavior(summonable=0, attackable=1, hostile=1, illusionable=0, convinceable=0, pushable=0, pushItems=1, pushCreatures=0, targetDistance=1, runOnHealth=0)
+death_blob.walkAround(energy=1, fire=1, poison=0)
+death_blob.setImmunity(paralyze=0, invisible=0, lifedrain=1, drunk=1)
