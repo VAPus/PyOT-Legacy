c4ebc31f041e50f45b441c46ba83b2c4bae4eea9
Stian Andreassen <stian@tech1.org>
2011-08-20 02:13:25 +0000

    Converted ezo map from soul4soul, converted from a server as i was using my own memory for something else, therefore, untested

--- data/map/info.py
+++ data/map/info.py
@@ -1,8 +1,8 @@
-width = 572
-height = 500
+width = 2048
+height = 2048
 author = 'OTBMXML2sec generator'
-description = 'SimOne MapEditor 0.5.0'
+description = 'SimOne MapEditor 0.5.5'
 sectorSize = (32, 32)
-towns = {1: ('Tharkas', [100, 100, 7]), 2: ('Khal-Tar', [335, 382, 7])}
+towns = {1: ('Tattiland', [550, 504, 6])}
 waypoints = {}
-levels = (10, 0)
+levels = (14, 0)
