d4c32029054a65059db6ed0b601590b5fe2a91d8
Stian Andreassen <stian@tech1.org>
2011-08-25 00:06:17 +0000

    Fix the last of sentinellos typos and other errors

--- data/monsters/Animals/Terror Bird.py
+++ data/monsters/Animals/Terror Bird.py
@@ -11,4 +11,4 @@ Terror_Bird.setBehavior(summonable=490, attackable=1, hostile=1, illusionable=49
 Terror_Bird.voices("Carrah! Carrah!", "Gruuuh Gruuuh.", "CRAAAHHH!")
 Terror_Bird.setImmunity(0,0,0) # paralyze, invisible, lifedrain
 Terror_Bird.setDefense(15, fire=1.1, earth=1.1, energy=0.8, ice=0.8, holy=1.0, death=1.05, physical=1.0, drown=1.0)
-Terror_Bird.loot( ('feather headdress', 0.525), ('seed', 0.55), ('health potion', 1.625), ('worm', 4.25, 3), ('meat', 16.5, 3), ('gold coin', 26.35, 30), ('terrorbird beak', 9), ('colourful feather', 1.75) )
+Terror_Bird.loot( ('feather headdress', 0.525), ('seeds', 0.55), ('health potion', 1.625), ('worm', 4.25, 3), ('meat', 16.5, 3), ('gold coin', 26.35, 30), ('terrorbird beak', 9), ('colourful feather', 1.75) )
