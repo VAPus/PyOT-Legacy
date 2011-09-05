acfb0da51236e8b95a336ef6bc887d570828ed6d
Stian Andreassen <stian@tech1.org>
2011-09-05 15:36:46 +0000

    Fix a duplicate issue

--- data/scripts/depot.py
+++ data/scripts/depot.py
@@ -9,7 +9,8 @@ def openDepot(creature, thing, **k):
 
 def closeDepot(creature, thing, **k):
     if thing.depotId:
-        creature.depot[thing.depotId] = thing.container.items
+        creature.depot[thing.depotId] = thing.container.items[:]
+        thing.container.items = []
 
 scriptsystem.regFirst('use', depots, openDepot) # We got to register it first so we call it before container open
 scriptsystem.reg('close', depots, closeDepot)
