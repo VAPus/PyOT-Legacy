c9b1fc55118498118af0681b889e9cba044827c8
Stian Andreassen <stian@tech1.org>
2011-10-19 22:26:37 +0000

    Convert soul4soul's latest map changes (post #446 in the debug thread) SQL: New houses

--- data/map/info.py
+++ data/map/info.py
@@ -1,10 +1,10 @@
 width = 1309
-height = 1265
+height = 1285
 author = 'OTBMXML2sec generator'
 description = """Saved with Remere's Map Editor 2.1
-SimOne MapEditor 0.4.49
+ezo - pyOT map
 """
 sectorSize = (32, 32)
-towns = {1: ('main', [1000, 1000, 7]), 2: ('second', [1229, 1092, 6])}
+towns = {1: ('main', [1000, 1000, 7]), 2: ('Ezo Town', [1229, 1092, 6])}
 waypoints = {}
 levels = (14, 1)
