a1034d8f50d31b850984848396ffde55adb21218
soul4soul <soul4soul@localhost>
2011-10-20 22:13:59 +0000

    step 1: remove monsters

--- data/monsters/Insects/__init__.py
+++
@@ -1,9 +0,0 @@
-import glob, os
-
-__all__ = []
-for mod in glob.glob("data/monsters/Insects/*.py"):
-    modm = mod.split(os.sep)[-1].replace('.py', '')
-    if modm == "__init__":
-        continue
-
-    __all__.append(modm)
--- data/monsters/Insects/pink butterfly.py
+++
@@ -1,7 +0,0 @@
-butterfly = game.monster.genMonster("Butterfly", (213, 4993), "a butterfly")
-butterfly.setHealth(2)
-butterfly.bloodType(color="slime")
-butterfly.setSpeed(300)
-butterfly.setBehavior(summonable=0, hostile=0, illusionable=1, convinceable=0, pushable=0, pushItems=0, pushCreatures=0, targetDistance=1, runOnHealth=0)
-butterfly.walkAround(energy=1, fire=1, poison=1)
-#note 4313 is an old used butterfly corpse sprite or second stage of decay?
