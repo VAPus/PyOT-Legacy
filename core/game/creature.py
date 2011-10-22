a44715909e3598ca1377eecc5da87fe5faed532b
Stian Andreassen <stian@tech1.org>
2011-10-22 22:29:24 +0000

    Bloodfix fix?

--- core/game/creature.py
+++ core/game/creature.py
@@ -596,13 +596,13 @@ class Creature(object):
             
             
         if by and by.isPlayer():
-            by.message("%s loses %d hitpoint%s due to your attack." % (self.name().capitalize(), -1 * dmg, 's' if dmg < -1 else ''), 'MSG_DAMAGE_DEALT', value = -1 * dmg, color = textColor, pos=self.position)
+            by.message("%s loses %d hitpoint%s due to your attack." % (self.name().capitalize(), -1 * dmg, 's' if dmg < -1 else ''), 'MSG_DAMAGE_DEALT', value = -1 * dmg, color = textColor, pos=by.position)
 
         if self.isPlayer():
             if by:
-                self.message("You lose %d hitpoint%s due to an attack by %s." % (-1 * dmg, 's' if dmg < -1 else '', by.name().capitalize()), 'MSG_DAMAGE_RECEIVED', value = -1 * dmg, color = textColor, pos=by.position)
+                self.message("You lose %d hitpoint%s due to an attack by %s." % (-1 * dmg, 's' if dmg < -1 else '', by.name().capitalize()), 'MSG_DAMAGE_RECEIVED', value = -1 * dmg, color = textColor, pos=self.position)
             else:
-                self.message("You lose %d hitpoint%s." % (-1 * dmg, 's' if dmg < -1 else ''), 'MSG_DAMAGE_RECEIVED')
+                self.message("You lose %d hitpoint%s." % (-1 * dmg, 's' if dmg < -1 else ''), 'MSG_DAMAGE_RECEIVED', value = -1 * dmg, color = textColor, pos=self.position)
 
         elif not self.target and self.data["health"] < 1:
             self.follow(by) # If I'm a creature, set my target
