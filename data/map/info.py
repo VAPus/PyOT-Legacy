fdb178ae37b35217eea58668a82d175800661a87
stian <stian@localhost>
2012-01-15 16:21:30 +0100

    add some various checks to make the monsters skip fewer attacks

---
+++ data/map/info.py
@@ -0,0 +1,10 @@
+width = 1309
+height = 1285
+author = 'OTBMXML2sec generator'
+description = """Saved with Remere's Map Editor 2.1
+ezo - pyOT map
+"""
+sectorSize = (32, 32)
+towns = {1: ('main', [1000, 1000, 7]), 2: ('Ezo Town', [1229, 1092, 6])}
+waypoints = {}
+levels = (14, 1)
---
+++ data/monsters/Mammals/Ungulates/Sheep.py
@@ -0,0 +1,12 @@
+Sheep = game.monster.genMonster("Sheep", (14, 5991), "a sheep")
+Sheep.setTargetChance(0)
+Sheep.bloodType("blood")
+Sheep.setHealth(20)
+Sheep.setExperience(0)
+Sheep.setSpeed(200) #incorrect
+Sheep.walkAround(1,1,1) # energy, fire, poison
+Sheep.setBehavior(summonable=250, hostile=0, illusionable=1, convinceable=250, pushable=1, pushItems=0, pushCreatures=0, targetDistance=0, runOnHealth=20)
+Sheep.voices("Maeh")
+Sheep.setImmunity(0,0,0) # paralyze, invisible, lifedrain
+Sheep.setDefense(2, fire=1.1, earth=1.0, energy=1.0, ice=1.0, holy=1.0, death=1.0, physical=1.0, drown=1.0)
+Sheep.loot( ("meat", 67.5, 4), ("wool", 1.5) )
