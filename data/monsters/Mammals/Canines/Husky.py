decffab77d78df73366f005e0b57c32ee6072c13
soul4soul <soul4soul@localhost>
2012-01-22 15:26:07 -0500

    added a space after the comma in loot messages.

---
+++ data/monsters/Mammals/Canines/Husky.py
@@ -0,0 +1,11 @@
+Husky = game.monster.genMonster("Husky", (258, 7316), "a husky")
+Husky.setTargetChance(10)
+Husky.bloodType("blood")
+Husky.setHealth(140)
+Husky.setExperience(0)
+Husky.setSpeed(264) #correct
+Husky.walkAround(1,1,1) # energy, fire, poison
+Husky.setBehavior(summonable=420, hostile=1, illusionable=1, convinceable=420, pushable=1, pushItems=0, pushCreatures=0, targetDistance=1, runOnHealth=100)
+Husky.voices("Yoooohuuuu!", "Grrrrrrr", "Ruff, ruff!")
+Husky.setImmunity(0,0,0) # paralyze, invisible, lifedrain
+Husky.setDefense(2, fire=1.0, earth=1.0, energy=1.0, ice=1.0, holy=1.0, death=1.0, physical=1.0, drown=1.0)
---
+++ data/scripts/actions/door_locked.py
@@ -0,0 +1,40 @@
+lockedDoors = 1209, 1212, 1231, 1234, 1249, 1252, 3535, 3544, 4913, 4616, 5098, 5107, 5116, 5125, 5134, 5137, 5140, 5143, 5278, 5281, 5732, 5735,\
+                6192, 6195, 6249, 6252, 6891, 6900, 7033, 7042, 8541, 8544, 9165, 9168, 9267, 9270, 10268, 10271, 10468, 10477
+                
+keys = range(2086, 2092+1)
+
+def onUseDoor(creature, thing, position, **k):
+    if not thing.actions:
+        # Shouldn't happend
+        print "Bad door on %s. It might bug" % str(position)
+        transformItem(thing, thing.itemId+2, position)
+    else:
+        if "houseDoor" in thing.actions:
+            houseId = game.map.getTile(position).houseId
+            if creature.data["id"] == game.house.houseData[houseId].owner or creature.data["id"] in game.house.houseData[houseId].data["subowners"]:
+                transformItem(thing, thing.itemId+2, position)
+                return
+        creature.message("It is locked.")
+
+def onUseKey(creature, thing, onThing, onPosition, **k):
+    if not onThing.actions or not onThing.itemId in lockedDoors or not onThing.itemId-1 in lockedDoors or not onThing.itemId-2 in lockedDoors:
+        return
+    
+    canOpen = False
+    for aid in thing.actions:
+        if aid in onThing.actions:
+            canOpen = True
+            
+    if not canOpen:
+        creature.message("The key does not match.")
+        return
+        
+    if onThing.itemId in lockedDoors:
+        transformItem(onThing, onThing.itemId+2, onPosition)
+    elif onThing.itemId-2 in lockedDoors:
+        transformItem(onThing, onThing.itemId-2, onPosition)
+    else:
+        transformItem(onThing, onThing.itemId-1, onPosition)
+
+reg('use', lockedDoors, onUseDoor)
+reg('useWith', keys, onUseKey)
