a68b6d78fa9e728df98044e12de264dc9ac18336
soul4soul <soul4soul@localhost>
2012-01-30 18:35:22 -0500

    added all? the distance attacks to creatures

--- data/monsters/Humanoids/Orcs/Orc Leader.py
+++ data/monsters/Humanoids/Orcs/Orc Leader.py
@@ -1,4 +1,3 @@
-
 orc_leader = game.monster.genMonster("Orc Leader", (59, 6001), "an orc leader")
 orc_leader.setHealth(450, healthmax=450)
 orc_leader.bloodType(color="blood")
@@ -10,4 +9,5 @@ orc_leader.walkAround(energy=1, fire=0, poison=1)
 orc_leader.setImmunity(paralyze=1, invisible=1, lifedrain=0, drunk=1)
 orc_leader.voices("Ulderek futgyr human!")
 orc_leader.regMelee(185)
+orc_leader.regDistance(70, ANIMATION_THROWINGKNIFE, game.monster.chance(21))
 orc_leader.loot( ("plate shield", 9.75), ("throwing knife", 21.0, 4), (2148, 100, 35), ("fish", 28.75), ("brown mushroom", 8.25), ("orc leather", 18.5), ("royal spear", 2.5), ("skull belt", 2.5), ("longsword", 2.0), ("health potion", 1.0), ("sword ring", 3.0), ("plate armor", 0.75), ("brass legs", 2.5), ("broadsword", 0.75), ("scimitar", 1.25), ("orc tooth", 0.75), ("plate legs", 0.5), ("warrior helmet", 0.0025) )
