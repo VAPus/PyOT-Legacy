0600efbcfce7603e05addf7c6bc2959096ee7cf8
soul4soul <soul4soul@localhost>
2012-03-22 22:08:11 -0400

    and to client 945 we go I suggest using clients 9.4 to 9.45 for the time being. clients below that arent getting much support right now.

--- config.py
+++ config.py
@@ -39,9 +39,9 @@ letGameServerRunTheLoginServer = True
 
 # Versions:
 versionMin = 860
-versionMax = 944
-versionError = "You must use 8.6-9.44"
-supportProtocols = (860, 861, 862, 870, 910, 920, 931, 940, 941, 942, 943, 944) # We support these main branches + compatible protocols to them
+versionMax = 945
+versionError = "You must use 8.6-9.45"
+supportProtocols = (860, 861, 862, 870, 910, 920, 931, 940, 941, 942, 943, 944, 945) # We support these main branches + compatible protocols to them
 
 # MOTD
 motd = "PyOT, it's alive!"
