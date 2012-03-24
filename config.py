827949403ac41f43b03431251699dd578bb5d3d9
soul4soul <soul4soul@localhost>
2012-03-24 11:02:24 -0400

    basic 946 support

--- config.py
+++ config.py
@@ -39,9 +39,9 @@ letGameServerRunTheLoginServer = True
 
 # Versions:
 versionMin = 860
-versionMax = 945
-versionError = "You must use 8.6-9.45"
-supportProtocols = (860, 861, 862, 870, 910, 920, 931, 940, 941, 942, 943, 944, 945) # We support these main branches + compatible protocols to them
+versionMax = 946
+versionError = "You must use 8.6-9.46"
+supportProtocols = (860, 861, 862, 870, 910, 920, 931, 940, 941, 942, 943, 944, 945, 946) # We support these main branches + compatible protocols to them
 
 # MOTD
 motd = "PyOT, it's alive!"
--- core/game/protocol.py
+++ core/game/protocol.py
@@ -1,7 +1,7 @@
 from twisted.python import log
 import sys
 
-protocolsAvailable = (860, 861, 862, 870, 910, 920, 931, 940, 941, 942, 943, 944, 945)
+protocolsAvailable = (860, 861, 862, 870, 910, 920, 931, 940, 941, 942, 943, 944, 945, 946)
 protocolsUsed = {}
 
 def getProtocol(version):
