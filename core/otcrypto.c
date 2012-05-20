7e0aba975e4988abbba47ba2e82effb958ab9617
stian <stian@localhost>
2012-05-20 04:21:16 +0200

    A (test) branch for people to review that it doesn't cause any duplicates or whatever other issues we've traditionally had when monster random walker have been enabled

---
+++ core/otcrypto.c
@@ -0,0 +1,159 @@
+#include <Python.h>
+#include <openssl/rsa.h>
+#include <openssl/bn.h>
+#include <openssl/err.h>
+#include <stdint.h>
+
+
+RSA* g_RSA;
+char gotkey;
+
+static PyObject* setkeys(PyObject* self, PyObject* args) {
+        if(gotkey)
+                Py_RETURN_NONE;
+
+        const char* n;
+        const char* e;
+        const char* d;
+        const char* p;
+        const char* q;
+        if (!PyArg_ParseTuple(args, "sssss", &n, &e, &d, &p, &q))
+                return NULL;
+
+        int retno;
+        retno = BN_dec2bn(&g_RSA->n, n);
+        if(retno)
+                retno = BN_dec2bn(&g_RSA->e, e);
+        if(retno)
+                retno = BN_dec2bn(&g_RSA->d, d);
+        if(retno)
+                retno = BN_dec2bn(&g_RSA->p, p);
+        if(retno)
+                retno = BN_dec2bn(&g_RSA->q, q);
+
+        ERR_load_crypto_strings();
+
+        if(retno && RSA_check_key(g_RSA)) {
+        } else {
+                printf(ERR_error_string(ERR_get_error(), NULL));
+                printf("\n");          
+        }
+ 
+        gotkey = 1;
+        Py_RETURN_NONE;
+}
+
+static PyObject* decryptRSA(PyObject* self, PyObject* args) {
+        const unsigned char* stream;
+        int length;
+
+        if (!PyArg_ParseTuple(args, "s#", &stream, &length))
+                return NULL;
+
+
+        length = RSA_private_decrypt(128, (unsigned char*)stream, (unsigned char*)stream, g_RSA, RSA_NO_PADDING);
+
+        
+        if(ERR_get_error()) {
+                printf(ERR_error_string(ERR_get_error(), NULL));
+                printf("\n");
+                return PyBytes_FromStringAndSize(NULL, 0);
+        }
+        return PyBytes_FromStringAndSize((char*)stream, length);
+}
+
+static PyObject* encryptXTEA(PyObject* self, PyObject* args) {
+        PyObject* streamObj;
+        char* stream;
+        uint32_t k[4];
+        Py_ssize_t length = 0;
+
+        if (!PyArg_ParseTuple(args, "O(kkkk)", &streamObj, &k[0], &k[1], &k[2], &k[3]))
+                return NULL;
+
+        PyString_AsStringAndSize(streamObj, &stream, &length);
+        uint8_t pad = 8 - (length % 8);
+        char* mbuffer = PyMem_New(char, (length + pad));
+
+        memcpy(mbuffer, stream, length);
+       
+        if ((length % 8)) {
+                memset((void*)&mbuffer[length], 0x33, pad);
+                length += pad;
+        }
+        //Py_BEGIN_ALLOW_THREADS
+        uint32_t* buffer = (uint32_t*)mbuffer;
+        int pos = 0;
+        uint8_t i;
+        while(pos < length / 4) {
+                uint32_t v0=buffer[pos], v1=buffer[pos + 1], sum=0;
+                for (i=0; i < 32; i++) {
+                        v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + k[sum & 3]);
+                        sum += 0x9E3779B9;
+                        v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + k[(sum>>11) & 3]);
+                }
+                buffer[pos]=v0; buffer[pos + 1]=v1;
+                pos += 2;
+        }
+        //Py_END_ALLOW_THREADS
+        PyObject* res = PyBytes_FromStringAndSize(mbuffer, length);
+        PyMem_Del(mbuffer);
+        return res;
+}
+ 
+static PyObject* decryptXTEA(PyObject* self, PyObject* args) {
+        PyObject* streamObj;
+	char* stream;
+        uint32_t k[4];
+	Py_ssize_t length = 0;
+	
+
+        if (!PyArg_ParseTuple(args, "O(kkkk)", &streamObj, &k[0], &k[1], &k[2], &k[3]))
+                return NULL;
+	PyString_AsStringAndSize(streamObj, &stream, &length);
+        //Py_BEGIN_ALLOW_THREADS
+        uint32_t* buffer = (uint32_t*)stream;
+        
+        uint8_t i;
+        int pos = 0;
+        while(pos < (length / 4)) {
+                uint32_t v0=buffer[pos], v1=buffer[pos + 1], sum=0x9E3779B9 * 32;
+                for (i=0; i < 32; i++) {
+                        v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + k[(sum>>11) & 3]);
+                        sum -= 0x9E3779B9;
+                        v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + k[sum & 3]);
+                }
+                buffer[pos]=v0; buffer[pos + 1]=v1;
+                pos += 2;
+        }
+        //Py_END_ALLOW_THREADS
+        return PyBytes_FromStringAndSize(stream, length);
+}
+
+
+static PyMethodDef otcryptoMethods[] = {
+        {"setkeys", setkeys, METH_VARARGS, "Initialize OpenSSL"},
+        {"decryptRSA", decryptRSA, METH_VARARGS, "Decrypts the string"},
+        {"decryptXTEA", decryptXTEA, METH_VARARGS, "Decrypts the string"},
+        {"encryptXTEA", encryptXTEA, METH_VARARGS, "Encrypts the string"},
+        {NULL, NULL, 0, NULL}
+};
+
+/** Used by Python3
+static struct PyModuleDef otrsamodule = {
+        PyObject_HEAD_INIT(NULL),
+        "otrsa",  --  name of module
+        NULL, -- module documentation, may be NULL
+        -1, --   size of per-interpreter state of the module, or -1 if the module keeps state in global variables.
+        otrsaMethods
+};
+*/
+
+/*PyMODINIT_FUNC
+PyInit_otrsa(void)*/
+PyMODINIT_FUNC initotcrypto(void) {
+        gotkey = 0;
+        g_RSA = RSA_new();
+        (void)Py_InitModule("otcrypto", otcryptoMethods);
+}
+
---
+++ data/monsters/Humans/Sorcerers/Energized Raging Mage.py
@@ -0,0 +1,14 @@
+#mostly unknown
+#always around not really a boss
+energized_raging_mage = genMonster("Energized Raging Mage", (423, 5995), "the energized raging mage") #unknown corpse
+energized_raging_mage.setHealth(4000)
+energized_raging_mage.bloodType(color="blood")
+energized_raging_mage.setDefense(armor=30, fire=1, earth=1, energy=0, ice=1, holy=1, death=1, physical=1, drown=1)
+energized_raging_mage.setExperience(0)
+energized_raging_mage.setSpeed(200)
+energized_raging_mage.setBehavior(summonable=0, hostile=1, illusionable=0, convinceable=0, pushable=0, pushItems=1, pushCreatures=1, targetDistance=1, runOnHealth=800)
+energized_raging_mage.walkAround(energy=0, fire=0, poison=0)
+energized_raging_mage.setImmunity(paralyze=1, invisible=1, lifedrain=1, drunk=1)
+energized_raging_mage.voices("Behold the all permeating powers I draw from this gate!!", "ENERGY!!", "I FEEL, I FEEEEEL... OMNIPOTENCE!!", "MWAAAHAHAAA!! NO ONE!! NO ONE CAN DEFEAT MEEE!!!")
+energized_raging_mage.summon("Golden Servant", 10)
+energized_raging_mage.maxSummons(1)
