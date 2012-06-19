0fc6ace1a63a0c05b3a05638541f1e1b980c4fe9
stian <stian@localhost>
2012-06-19 16:27:18 +0200

    Slightly better otcrypto_python.py

--- core/otcrypto_python.py
+++ core/otcrypto_python.py
@@ -1,5 +1,5 @@
 import config
-import struct
+from struct import pack, unpack
 
 D = int(config.RSAKeys["d"])
 N = int(config.RSAKeys["n"])
@@ -20,36 +20,35 @@ def decryptXTEA(stream, k):
     buffer = ""
     pos = 0
     while pos < (len(stream)):
-        v0, v1 = struct.unpack("<2L", stream[pos:pos+8])
+        v0, v1 = unpack("<2L", stream[pos:pos+8])
         
         sum = (0x9E3779B9 * 32) & 0xffffffff
         for i in xrange(32):
             v1 = (v1 - (((v0<<4 ^ v0>>5) + v0) ^ (sum + k[sum>>11 & 3]))) & 0xffffffff
             sum = (sum - 0x9E3779B9) & 0xffffffff
             v0 = (v0 - (((v1<<4 ^ v1>>5) + v1) ^ (sum + k[sum & 3]))) & 0xffffffff
-        buffer += struct.pack("<2L", v0, v1)
+        buffer += pack("<2L", v0, v1)
         pos += 8
 
     return buffer
 
 def encryptXTEA(stream, k):
-    
     buffer = ""
     pos = 0
 
-    pad = (8 - (len(stream) % 8)) % 8
+    pad = len(stream) % 8
     if pad:
         stream += "\x33" * pad
 
     pos = 0
     while pos < (len(stream)):
-        v0, v1= struct.unpack("<2L", stream[pos:pos+8])
+        v0, v1= unpack("<2L", stream[pos:pos+8])
         sum = 0
         for i in xrange(32):
             v0 = (v0 + (((v1<<4 ^ v1>>5) + v1) ^ (sum + k[sum & 3]))) & 0xffffffff
             sum = (sum + 0x9E3779B9) & 0xffffffff
             v1 = (v1 + (((v0<<4 ^ v0>>5) + v0) ^ (sum + k[sum>>11 & 3]))) & 0xffffffff
-        buffer += struct.pack("<2L", v0, v1)
+        buffer += pack("<2L", v0, v1)
         pos += 8
 
 
