1cba19ec01679f260a3bd78448bdd899195711f3
stian <stian@localhost>
2012-09-23 02:02:04 +0200

    Start working on the war system

---
+++ data/scripts/actions/inkwell.py
@@ -0,0 +1,22 @@
+@register("useWith", 2600)
+def onUseWith(creature, thing, position, onThing, onPosition, **k):
+    rand = random.randint(1, 2)
+    if onThing.itemId == 7528:
+        onThing.transform(7529, onPosition)
+    elif onThing.itemId == 7529:
+        if rand == 1:
+            onThing.transform(7530, onPosition)
+        elif rand == 2:
+            onThing.transform(7531, onPosition)
+    elif onThing.itemId == 7530:
+        if rand == 1:
+            onThing.transform(7532, onPosition)
+        elif rand == 2:
+            onThing.transform(7533, onPosition)
+    elif onThing.itemId == 7532:
+        if rand == 1:
+            onThing.transform(7534, onPosition)
+        elif rand == 2:
+            onThing.transform(7535, onPosition)
+    
+    return True
