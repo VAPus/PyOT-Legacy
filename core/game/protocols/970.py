c128e9afe133cf3e5f5bf06ce94e40b9819a68b7
stian <stian@localhost>
2012-10-21 01:33:42 +0200

    Probable fix for #63 about weapons debug.

--- core/game/protocols/970.py
+++ core/game/protocols/970.py
@@ -15,12 +15,6 @@ class Packet(base.BasePacket):
     def magicEffect(self, pos, type):
         self.data += pack("<BHHBB", 0x83, pos.x, pos.y, pos.z, type)
    
-    # Shoot
-    def shoot(self, fromPos, toPos, type):
-        self.uint8(0x85)
-        self.position(fromPos)
-        self.position(toPos)
-        self.uint16(type)
         
 class Protocol(base.BaseProtocol):
     Packet = Packet
