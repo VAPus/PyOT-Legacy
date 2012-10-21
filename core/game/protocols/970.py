0c1d432071e94203c410ebee47763e7e54b6eef8
stian <stian@localhost>
2012-10-21 02:26:08 +0200

    0.5s is overkill. Do 10ms. This makes tests much faster.

---
+++ core/game/protocols/970.py
@@ -0,0 +1,20 @@
+# This is a shadow of the main branch, 9.51
+import base
+from struct import pack
+
+provide = []
+
+def vertify(): return True
+
+
+class Packet(base.BasePacket):
+    maxOutfits = 33
+    maxMounts = 31
+
+    # Magic Effect
+    def magicEffect(self, pos, type):
+        self.data += pack("<BHHBB", 0x83, pos.x, pos.y, pos.z, type)
+   
+        
+class Protocol(base.BaseProtocol):
+    Packet = Packet
---
+++ test/internals/test_conditions.py
@@ -0,0 +1,28 @@
+from test.framework import FrameworkTestGame
+
+class TestCondition(FrameworkTestGame):
+    def test_addCondition(self):
+        condition = Condition(CONDITION_FIRE, length=10, damage=1)
+        self.player.condition(condition)
+
+        self.assertTrue(self.player.conditions)
+        self.assertTrue(condition.creature)
+
+    def test_hascondition(self):
+        condition = Condition(CONDITION_FIRE, length=10, damage=1)
+        self.player.condition(condition)
+
+        self.assertTrue(self.player.hasCondition(CONDITION_FIRE))
+
+    def test_getcondition(self):
+        condition = Condition(CONDITION_FIRE, length=10, damage=1)
+        self.player.condition(condition)
+
+        self.assertEqual(self.player.getCondition(CONDITION_FIRE), condition)
+
+    def test_copycondition(self):
+        condition = Condition(CONDITION_FIRE, length=10, damage=1)
+
+        condition2 = condition.copy()
+
+        self.assertEqual(condition.type, condition2.type)
