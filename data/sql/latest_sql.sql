e8c36e5c34bbf827bc3dfa65d51a67c6db503db8
stian <stian@localhost>
2012-10-28 17:34:18 +0100

    Market history SQL.

--- data/sql/latest_sql.sql
+++ data/sql/latest_sql.sql
@@ -25152,3 +25152,12 @@ CREATE TABLE IF NOT EXISTS `market_offers` (
 buy',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `market_history` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `offer_id` int(11) unsigned NOT NULL,
+  `player_id` int(11) unsigned NOT NULL,
+  `amount` int(11) unsigned NOT NULL,
+  `time` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
