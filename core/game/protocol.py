0c506c8105a03eff88804a9785b047f3a4b857a2
stian <stian@localhost>
2012-11-04 18:39:56 +0100

    A branch to try and kill the sid and see what happends.

---
+++ core/game/protocol.py
@@ -0,0 +1,30 @@
+from twisted.python import log
+import sys, os
+
+protocolsAvailable = (860, 861, 862, 870, 910, 920, 931, 940, 941, 942, 943, 944, 945, 946, 951, 952, 953, 954, 960, 962, 963, 970)
+protocolsUsed = {}
+
+def getProtocol(version):
+    if not protocolsUsed:
+        loadProtocol(version)
+    try:
+        return protocolsUsed[version]
+    except:
+        log.msg("Protocol %d unsupported" % version)
+    return None
+
+def loadProtocol(version):
+    if "_trial_temp" in os.getcwd():
+        os.chdir("..")
+        
+    if not version in protocolsAvailable:
+        log.msg("Protocol (Base) %d doesn't exist!" % version)
+        return
+        
+    protocol = __import__('game.protocols.%d' % version, globals(), locals())
+    protocol = sys.modules['game.protocols.%d' % version]
+            
+    protocol.verify()
+    protocolsUsed[version] = protocol.Protocol()
+    for x in protocol.provide:
+        protocolsUsed[x] = protocolsUsed[version]
---
+++ data/monsters/Mammals/Apes/Kongra.py
@@ -0,0 +1,13 @@
+Kongra = genMonster("Kongra", (116, 6043), "a Kongra")
+Kongra.setTargetChance(10)
+Kongra.bloodType("blood")
+Kongra.setHealth(340)
+Kongra.setExperience(115)
+Kongra.setSpeed(184) # Correct
+Kongra.walkAround(1,1,1) # energy, fire, poison
+Kongra.setBehavior(summonable=0, hostile=1, illusionable=1, convinceable=0, pushable=0, pushItems=1, pushCreatures=1, targetDistance=1, runOnHealth=10)
+Kongra.voices("Ungh! Ungh!", "Hugah!", "Huaauaauaauaa!")
+Kongra.setImmunity(0,0,0) # paralyze, invisible, lifedrain
+Kongra.setDefense(20, fire=0.8, earth=0.9, energy=0.95, ice=1.15, holy=1.0, death=1.05, physical=1.0, drown=1.0)
+Kongra.regMelee(60)
+Kongra.loot( ("protection amulet", 0.75), (2148, 100, 40), ("banana", 51.75, 12), ("plate armor", 1.0), ("ape fur", 1.0, 3), (12427, 5.25), ("health potion", 0.5), ("power ring", 0.25), ("club ring", 0.25) )
