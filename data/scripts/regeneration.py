593d64d192e823b60f319081c60cca547c33aa61
stian <stian@localhost>
2012-11-21 18:02:55 +0100

    Small fix.

--- data/scripts/regeneration.py
+++ data/scripts/regeneration.py
@@ -3,8 +3,8 @@ if config.regenerationTresshold > 0:
         if player.data['health'] < config.regenerationTresshold:
             player.setHealth(config.regenerationTresshold)
 
-    @register('hit')
-    def regen(creature, damage):
-        if (creature.isPlayer() and creature.data['health']-damage < config.regenerationTresshold \
+    @register('hit', 'player')
+    def regen(creature, damage, **k):
+        if (creature.data['health']-damage < config.regenerationTresshold \
            and not creature.hasCondition(CONDITION_INFIGHT) and not (creature.extraIcons & CONDITION_PROTECTIONZONE)):
             callLater(config.regenerationDelay, regenerator, creature)
