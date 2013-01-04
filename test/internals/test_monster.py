03f1cdc96091d231a76934cae9cf25a2516c2e2b
stian <stian@localhost>
2013-01-04 02:03:54 +0100

    Fix remaining test_monster using an ignore list, #128, #129

--- test/internals/test_monster.py
+++ test/internals/test_monster.py
@@ -2,9 +2,15 @@ from test.framework import FrameworkTestGame
 
 class TestMonster(FrameworkTestGame):
     def test_corpses(self):
+        # For "special" monsters with no corpse or so.
+        ignore = ("Northern Pike", "Rift Worm", "Insect Swarm", "Fish", "Slime", "Butterfly", "Son Of Verminor")
+
         printer = self.fail
         #printer = sys.__stdout__.write
         for monsterName in game.monster.monsters:
+            if monsterName in ignore:
+                continue
+
             monster = getMonster(monsterName)
             if not monster.data['corpse']:
                 printer("[ERROR] Monster %s got no corpse!\n" % monsterName)
@@ -16,8 +22,8 @@ class TestMonster(FrameworkTestGame):
 
             else:
                 name = item.name.lower()
-                if not "dead" in name and not "slain" in name and not "undead" in name and not "remains" in name:
-                    printer("[WARNING] Monster %s (corpse: %d) doesn't have dead/slain/undead/remains in it's name, likely invalid\n" % (monsterName, corpse))
+                if not "dead" in name and not "slain" in name and not "undead" in name and not "remains" in name and not "lifeless" in name:
+                    printer("[WARNING] Monster %s (corpse: %d) doesn't have dead/slain/undead/remains/lifeless in it's name, likely invalid\n" % (monsterName, corpse))
                     continue
 
             if not item.corpseType:
