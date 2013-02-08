dfa24d68825dfe5be436ac52911452fedf8134ea
stian <stian@localhost>
2013-02-08 02:31:14 +0100

    Extend the test

--- core/service/webgame.py
+++ core/service/webgame.py
@@ -1,8 +1,11 @@
 from twisted.internet.protocol import Protocol, Factory
 
 class ClientProtocol(Protocol):
+    def _delayed(self):
+        self.transport.write(" NOT!")
     def dataReceived(self, data):
         self.transport.write(data)
+        callLater(3, self._delayed)
 
 class ClientFactory(Factory):
     protocol = ClientProtocol
